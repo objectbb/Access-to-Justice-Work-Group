c385a335fba0603628dc7888e69444a14644aca0
Brian J. Burroughs <bburroughs@orionsmind.com>
2015-05-12 14:01:00 -0500

    adv sql query bug

--- js/app/main/app.js
+++ js/app/main/app.js
@@ -1,3 +1,15 @@
 (function() {
-    angular.module('taxidriver', ['ui.bootstrap', 'isteven-multi-select']);
-}());
+        angular.module('taxidriver', ['ui.bootstrap', 'isteven-multi-select']).
+        config(['$httpProvider', function($httpProvider) {
+                    // Intercept POST requests, convert to standard form encoding
+                    $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+                    $httpProvider.defaults.transformRequest.unshift(function(data, headersGetter) {
+                        var key, result = [];
+                        for (key in data) {
+                            if (data.hasOwnProperty(key)) {
+                                result.push(encodeURIComponent(key) + "=" + encodeURIComponent(data[key]));
+                            }
+                        }
+                        return result.join("&");
+                    })}]);
+                }());
--- js/app/main/services/fusiontables/reports.js
+++ js/app/main/services/fusiontables/reports.js
@@ -1,16 +1,23 @@
 (function() {
     angular.module('taxidriver').service('ReportService', ['$http', function($http) {
-        var key = "key=AIzaSyAm9yWCV7JPCTHCJut8whOjARd7pwROFDQ";
+        var key = "AIzaSyAm9yWCV7JPCTHCJut8whOjARd7pwROFDQ";
         return {
             request: function(query) {
-                      var url = "https://www.googleapis.com/fusiontables/v2/query?sql=";
+                var url = "https://www.googleapis.com/fusiontables/v2/query";
                 var urlrequest = url + query + "&" + key;
-                return $http.get(urlrequest);
+                var req = {
+                    method: 'POST',
+                    url: url,
+                    data: {
+                        key: key,
+                        sql: query
+                    }
+                }
+                return $http(req);
             },
             requestcolumns: function(id) {
-              var url = "https://www.googleapis.com/fusiontables/v2/tables/";
-
-                var urlrequest = url + id + "/columns?" + key;
+                var url = "https://www.googleapis.com/fusiontables/v2/tables/";
+                var urlrequest = url + id + "/columns?key=" + key;
                 return $http.get(urlrequest);
             }
         }
