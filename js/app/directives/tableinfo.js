e6ea721a3278db4d6d6b952ba59aa747b76242ca
Brian J. Burroughs <bburroughs@orionsmind.com>
2015-05-12 15:40:46 -0500

    update datatable css and bootstrap

--- js/app/directives/tableinfo.js
+++ js/app/directives/tableinfo.js
@@ -1,14 +1,13 @@
-(function() {
-    angular.module('taxidriver').directive('tableinfo', function() {
-  return {
-  	scope: {
-      title: '=title',
-      fusiontbl: '=fusiontbl',
-      columns: '=columns',
-      send: '=send'
-    },
-     templateUrl: 'js/app/directives/_tableinfo.html'
-  };
-});
-
+(function () {
+    angular.module('taxidriver').directive('tableinfo', function () {
+        return {
+            scope: {
+                title: '=title',
+                fusiontbl: '=fusiontbl',
+                columns: '=columns',
+                send: '=send'
+            },
+            templateUrl: 'js/app/directives/_tableinfo.html'
+        };
+    });
 }());
