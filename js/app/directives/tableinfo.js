738037c76fb26e0be1e7b438eeabd72878ed3f53
Brian J. Burroughs <bburroughs@orionsmind.com>
2015-05-14 15:07:16 -0500

    add map forreports that have address

--- js/app/directives/tableinfo.js
+++ js/app/directives/tableinfo.js
@@ -1,13 +1,18 @@
 (function () {
     angular.module('taxidriver').directive('tableinfo', function () {
         return {
+            controller: function ($scope) {
+                $scope.containsAddress = function (cols) {
+                    return _.any(cols,{'name': "Address"});
+                };
+            },
             scope: {
                 title: '=title',
                 fusiontbl: '=fusiontbl',
                 columns: '=columns',
                 send: '=send'
             },
-            templateUrl: 'js/app/directives/_tableinfo.html'
+            templateUrl: 'js/app/directives/_tableinfo.html',
         };
     });
 }());
--- js/app/main/app.js
+++ js/app/main/app.js
@@ -1,15 +1,16 @@
-(function() {
-        angular.module('taxidriver', ['ui.bootstrap', 'isteven-multi-select']).
-        config(['$httpProvider', function($httpProvider) {
-                    // Intercept POST requests, convert to standard form encoding
-                    $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-                    $httpProvider.defaults.transformRequest.unshift(function(data, headersGetter) {
-                        var key, result = [];
-                        for (key in data) {
-                            if (data.hasOwnProperty(key)) {
-                                result.push(encodeURIComponent(key) + "=" + encodeURIComponent(data[key]));
-                            }
-                        }
-                        return result.join("&");
-                    })}]);
-                }());
+(function () {
+    angular.module('taxidriver', ['ui.bootstrap', 'isteven-multi-select','leaflet-directive']).
+    config(['$httpProvider', function ($httpProvider) {
+        // Intercept POST requests, convert to standard form encoding
+        $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        $httpProvider.defaults.transformRequest.unshift(function (data, headersGetter) {
+            var key, result = [];
+            for (key in data) {
+                if (data.hasOwnProperty(key)) {
+                    result.push(encodeURIComponent(key) + "=" + encodeURIComponent(data[key]));
+                }
+            }
+            return result.join("&");
+        })
+    }]);
+}());
