ce60738f857d02dae04cedc357207993b87f8e3b
Brian J. Burroughs <bburroughs@orionsmind.com>
2015-05-22 18:14:14 -0500

    add grid per table

--- js/app/main/controller/app.js
+++ js/app/main/controller/app.js
@@ -1,7 +1,7 @@
-(function() {
+(function () {
     var dataTable;
     angular.module('taxidriver').controller('AppCtrl', ['$scope', '$q', 'ReportService', 'da',
-        function($scope, $q, ReportService, da) {
+        function ($scope, $q, ReportService, da) {
             $scope.sql = null;
             $scope.sqlrs = null;
             $scope.sqlcols = [];
@@ -11,73 +11,95 @@
             fusionmap["1An33ZqdkTpqMM1UjNy1cW0QDfAUhR0Hdtad0vkpJ"] = "vr";
             fusionmap["1UCyBNGAL7544gB8Se2QaPwRWRmsSZ0c5aeD2m6hJ"] = "tmht";
             fusionmap["14EXK6TvoG0XUY9PJzxUPfLTl5FjlsSEeidkA8mNV"] = "dfin";
-            var getcolumns = function(id) {
+            var getcolumns = function (id) {
                 var deferred = $q.defer();
                 var rs;
-                ReportService.requestcolumns(id).then(function(data, status) {
+                ReportService.requestcolumns(id).then(function (data, status) {
                     rs = deferred.resolve(_.sortByAll(data.data.items, 'name'));
-                }, function(updates) {
+                }, function (updates) {
                     deferred.update(updates);
                 });
                 return deferred.promise;
             }
             for (var key in fusionmap) {
-                (function(key) {
+                (function (key) {
                     var name = fusionmap[key];
-                    getcolumns(key).then(function(data) {
+                    getcolumns(key).then(function (data) {
                         $scope[name] = data;
                         da.createtable(name, data);
                     });
                 })(key);
             }
-            $scope.send = function(table, inputcols) {
-                var msgid = "#" + table + "msg";
+             $scope.sendadvsql = function (sql,tableid) {
+                var msg;
+                var msgid = "#" + tableid + "msg";
+                $(msgid).html("Processing...");
+               
+                sendsql(sql, tableid).then(function (data, status) {
+                    if (!data.error) {
+                        loaddatagrid(tableid, data);
+                        msg = "Results at the bottom..." + data.rows.length + " rows returned";
+                        $(msgid).html(msg);
+                    } else msg = data.error.errors[0].message;
+                    $(msgid).html(msg);
+                });
+                //da.loadtable(tableid, convertcolstojson(table, data));
+                //da.execute();
+            }
+            $scope.send = function (tableid, inputcols) {
+                var msg;
+                var msgid = "#" + tableid + "msg";
+                $(msgid).html("Processing...");
+                var query = buildquery(tableid, inputcols);
+                sendsql(query, tableid).then(function (data, status) {
+                    if (!data.error) {
+                        loaddatagrid(tableid, data);
+                        msg = "Results at the bottom..." + data.rows.length + " rows returned";
+                        $(msgid).html(msg);
+                    } else msg = data.error.errors[0].message;
+                    $(msgid).html(msg);
+                });
+                //da.loadtable(tableid, convertcolstojson(table, data));
+                //da.execute();
+            }
+            var buildquery = function (tableid, inputcols) {
                 var formcols = inputcols;
-                var cols = (_.map(formcols, function(item) {
+                var cols = (_.map(formcols, function (item) {
                     return "'" + item.name + "'";
                 })).join(',');
                 var colsquery = (formcols === null || formcols.length > 0) ? cols + ",count()" : " * ";
                 var groupbyquery = (formcols === null || formcols.length > 0) ? " group by " + cols : "";
-                var query = "select " + colsquery + " from " + table + groupbyquery;
-                var table = fusionmap[table];
-                $scope.sendsql(table, query, msgid);
+                return "select " + colsquery + " from " + tableid + groupbyquery;
             }
-            $scope.sendsql = function(table, query, msgid) {
-                $(msgid).html("Processing...");
-                ReportService.request(query).
-                success(function(data, status) {
-                    loaddatagrid(data);
-                    loadsqltables(table, data);
-                    $(msgid).html("Results at the bottom..." + data.rows.length + " rows returned");
+            var sendsql = function (query, tableid) {
+                var deferred = $q.defer();
+                ReportService.request(query).success(function (data, status) {
+                    return deferred.resolve(data);
                 }).
-                error(function(data, status) {
-                    var msg = data.error.errors[0].message;
-                    $(msgid).html("Error:" + msg);
+                error(function (data, status) {
+                    return deferred.resolve(data);
                 });
+                return deferred.promise;
             }
-            var loadsqltables = function(table, data) {
-                var jsondata = _.map(data.rows, function(n) {
+            var convertcolstojson = function (tableid, data) {
+                var jsondata = _.map(data.rows, function (n) {
                     var row = {};
                     var len = data.columns.length;
                     for (var i = 0; i < len; i++) row[data.columns[i]] = n[i];
                     return row;
                 });
-                da.loadtable(table, jsondata);
-               // runinfotable(table);
+                return jsondata;
             }
-            var runinfotable = function(table) {
-                    da.execute();
-            }
-            var loaddatagrid = function(dataset) {
+            var loaddatagrid = function (tableid, dataset) {
                 if (dataTable) {
                     dataTable.fnDestroy();
                 }
-                var cols = _.map(dataset.columns, function(n) {
+                var cols = _.map(dataset.columns, function (n) {
                     return {
                         'title': n
                     };
                 });
-                dataTable = $('#rstable').empty().dataTable({
+                dataTable = $('#' + tableid + "rstable").empty().dataTable({
                     destroy: true,
                     "data": dataset.rows,
                     "scrollY": "500px",
